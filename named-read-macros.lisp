60521e1d04c1fdb1ef8dea953ab370d7e8a90a3a
William Yao <williamyaoh@gmail.com>
2017-01-11 00:02:59 -0600

    with better ending syntax

--- named-read-macros.lisp
+++ named-read-macros.lisp
@@ -1,5 +1,45 @@
 (in-package #:named-read-macros)
 
+(defun readtable-case-transform-fn (&optional (readtable *readtable*))
+  "Return a function which will transform any string per the given
+   readtable's case-sensitivity."
+  (ecase (readtable-case readtable)
+    (:upcase #'string-upcase)
+    (:downcase #'string-downcase)
+    (:preserve #'identity)
+    (:invert (lambda (str)
+               (let ((alphas (remove-if-not #'alpha-char-p str)))
+                 (cond
+                   ((every #'upper-case-p alphas) (string-downcase str))
+                   ((every #'lower-case-p alphas) (string-upcase str))
+                   (:otherwise str)))))))
+
+(defun read-string-end (stream symbol)
+  (let ((end-name (coerce (string-downcase
+                           (concatenate 'string "END-" (symbol-name symbol)))
+                          'list))
+        (case-transform-fn (readtable-case-transform-fn)))
+    (with-output-to-string (contents)
+      (loop (let ((end-str (with-output-to-string (end-str)
+                             (loop with pos = end-name
+                                   while pos
+                                   for char = (read-char stream t nil t)
+                                   do (if (char= (char-downcase char) (car pos))
+                                          (progn
+                                            (write-char char end-str)
+                                            (setf pos (cdr pos)))
+                                          (progn
+                                            ;; wipe END-STR contents
+                                            (write-string (get-output-stream-string end-str)
+                                                          contents)
+                                            (write-char char contents)
+                                            (setf pos end-name)))))))
+              (if (and (string= (funcall case-transform-fn (subseq end-str 4))
+                                (symbol-name symbol))
+                       (char= (peek-char nil stream t nil t) #\) ))
+                  (progn (read-char) (return))
+                  (write-string end-str contents)))))))
+
 (defun read-maybe-read-macro (stream &optional char)
   (declare (ignorable char))
   (if (char= (peek-char t stream t nil t) #\) )
@@ -7,7 +47,8 @@
       (let ((head (read stream t nil t)))
         (if (and (symbolp head) (get head 'dispatch-macro))
             (progn (peek-char t stream t nil t)
-                   (funcall (get head 'dispatch-macro) stream))
+                   (with-input-from-string (stream* (read-string-end stream head))
+                     (funcall (get head 'dispatch-macro) stream*)))
             (cons head (read-delimited-list #\) stream t))))))
 
 (defmacro define (name &body body)
